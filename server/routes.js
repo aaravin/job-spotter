f5e119373e9a0d289384e583b201d49d7620d345
Timmy Luong <tcl.luong@gmail.com>
2015-07-14 17:10:12 -0700

    Remove unused controllers

--- server/routes.js
+++ server/routes.js
@@ -2,20 +2,3 @@ var express = require('express');
 var router = express.Router();
 var Promise = require('bluebird');
 
-router.get('/api/data/',function(req,res,next){
-  var combined = [];
-
-  mainController
-    .startDataExtraction(combined)
-      .then(function(combined) {
-        
-    }).then(function(combined) {
-
-    }).then(function(combined) {
-
-    }).then(function(combined) {
-
-    }).then(function(combined) {
-      mainController.sendBackData(combined, res, next);
-    });
-});
