d4659b4af00a9a140f700b4e25670813cfbd12e9
Timmy Luong <tcl.luong@gmail.com>
2015-07-15 11:59:24 -0700

    Add tests for database

--- test/ServerSpec.js
+++ test/ServerSpec.js
@@ -1,3 +1,4 @@
+var mysql = require('mysql');
 var expect = require('chai').expect;
 var request = require('request');
 
@@ -45,4 +46,92 @@ describe('', function() {
     });
   });
 
+  describe("Database Tests", function() {
+    var TABLES_COUNT = 8;
+    
+    var dbConnection;
+
+    beforeEach(function(done) {
+      dbConnection = mysql.createConnection({
+        user: "root",
+        password: "",
+        database: "jobspotter"
+      });
+      dbConnection.connect();
+      done();
+    });
+
+    afterEach(function() {
+      dbConnection.end();
+    });
+
+    it("Should have tables in the database", function(done) {
+      var queryString = "SHOW TABLES";
+      var queryArgs = [];
+
+      dbConnection.query(queryString, queryArgs, function(err, results) {
+        expect(results).to.not.equal(undefined);
+        // console.log('results.length', results.length);
+        done();
+      });
+    });
+
+    it("Should have the correct number of tables in the database", function(done) {
+      var queryString = "SHOW TABLES";
+      var queryArgs = [];
+
+      dbConnection.query(queryString, queryArgs, function(err, results) {
+        expect(results.length).to.equal(TABLES_COUNT);
+        // console.log('results.length', results.length);
+        done();
+      });
+    });
+
+    it("Should have table named links", function(done) {
+      var queryString = "SELECT * FROM links";
+      var queryArgs = [];
+
+      dbConnection.query(queryString, queryArgs, function(err, results) {
+        expect(results).to.not.equal(undefined);
+        done();
+      });
+    });
+
+    it("Should have columns in table named links", function(done) {
+      var queryString = "SHOW COLUMNS FROM links";
+      var queryArgs = [];
+
+      dbConnection.query(queryString, queryArgs, function(err, results) {
+        expect(results.length).to.be.greaterThan(0);
+        // console.log('results', results);
+        done();
+      });
+    });
+
+    it("Should have a primary key in the first column in table named links", function(done) {
+      var queryString = "SHOW COLUMNS FROM links";
+      var queryArgs = [];
+
+      dbConnection.query(queryString, queryArgs, function(err, results) {
+        expect(results[0].Key).to.be.equal('PRI');
+        console.log('results[0].Key', results[0].Key);
+        done();
+      });
+    });
+
+    it("Should have data rows in table named links", function(done) {
+      var queryString = "SELECT * FROM links";
+      var queryArgs = [];
+
+      dbConnection.query(queryString, queryArgs, function(err, results) {
+        expect(results.length).to.be.greaterThan(0);
+        // console.log('results.length', results.length);
+        done();
+      });
+    });
+
+  });
+
 });
+
+
