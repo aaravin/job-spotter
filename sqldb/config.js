bd81f4a2028b76461469f15c9ff8219b5eb659f6
Timmy Luong <tcl.luong@gmail.com>
2015-07-16 14:36:41 -0700

    Add schemas to match new database - also renamed former database to old_database as backup

--- sqldb/config.js
+++ sqldb/config.js
@@ -19,106 +19,118 @@ var knex = require('knex')({
 
 var db = require('bookshelf')(knex);
 
-// db.knex.schema.hasTable('users').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('users', function (table) {
-//       table.increments('id').primary();
-//       table.string('username', 100).unique();
-//       table.string('password', 255);
-//       table.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
+db.knex.schema.hasTable('Jobs').then(function (exists) {
+  if (!exists) {
+    db.knex.schema.createTable('Jobs', function (table) {
+      table.uuid('id').primary();
+      table.string('name', 255);
+      table.text('description');
+      table.float('equity_min');
+      table.float('equity_max');
+      table.float('salary_min');
+      table.float('salary_max');
+      table.string('currency', 255);
+      table.timestamps();
+    }).then(function (table) {
+      console.log('Created Table', table);
+    });
+  }
+});
 
-// db.knex.schema.hasTable('titles').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('titles', function (table) {
-//       table.increments('id').primary();
-//       table.string('title', 100);
-//       table.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
+db.knex.schema.hasTable('Locs').then(function (exists) {
+  if (!exists) {
+    db.knex.schema.createTable('Locs', function (table) {
+      table.uuid('id').primary();
+      table.string('name', 255);
+      table.timestamps();
+    }).then(function (table) {
+      console.log('Create Table', table);
+    });
+  }
+});
 
-// db.knex.schema.hasTable('companies').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('companies', function (table) {
-//       table.increments('id').primary();
-//       table.string('name', 100);
-//       table.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
+db.knex.schema.hasTable('Roles').then(function (exists) {
+  if (!exists) {
+    db.knex.schema.createTable('Roles', function (table) {
+      table.uuid('id').primary();
+      table.string('name', 255);
+      table.timestamps();
+    }).then(function (table) {
+      console.log('Create Table', table);
+    });
+  }
+});
 
-// db.knex.schema.hasTable('locations').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('locations', function (table) {
-//       table.increments('id').primary();
-//       table.string('city', 100);
-//       // table.string('state', 100);
-//       // table.string('country', 50);
-//       // table.float('latitude', 10);
-//       // table.float('longitude', 10);
-//       table.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
+db.knex.schema.hasTable('Skills').then(function (exists) {
+  if (!exists) {
+    db.knex.schema.createTable('Skills', function (table) {
+      table.uuid('id').primary();
+      table.string('name', 255);
+      table.timestamps();
+    }).then(function (table) {
+      console.log('Create Table', table);
+    });
+  }
+});
 
-// db.knex.schema.hasTable('links').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('links', function (table) {
-//       table.increments('id').primary();
-//       table.string('link', 255);
-//       // table.string('description', 255); //only show first 255 chars of job description
-//       table.integer('title_id').unsigned().references('id').inTable('titles');
-//       table.integer('company_id').unsigned().references('id').inTable('companies');
-//       table.integer('location_id').unsigned().references('id').inTable('locations');
-//       table.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
+db.knex.schema.hasTable('Startups').then(function (exists) {
+  if (!exists) {
+    db.knex.schema.createTable('Startups', function (table) {
+      table.uuid('id').primary();
+      table.string('name', 255);
+      table.timestamps();
+    }).then(function (table) {
+      console.log('Create Table', table);
+    });
+  }
+});
 
-// db.knex.schema.hasTable('titles_companies').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('titles_companies', function (table) {
-//       table.integer('title_id').unsigned().references('id').inTable('titles');
-//       table.integer('company_id').unsigned().references('id').inTable('companies');
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
+db.knex.schema.hasTable('loc_job').then(function (exists) {
+  if (!exists) {
+    db.knex.schema.createTable('loc_job', function (table) {
+      table.timestamps();
+      table.uuid('Job_rowId').primary();
+      table.uuid('Loc_rowId').primary();
+    }).then(function (table) {
+      console.log('Create Table', table);
+    });
+  }
+});
 
-// db.knex.schema.hasTable('titles_locations').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('titles_locations', function (table) {
-//       table.integer('title_id').unsigned().references('id').inTable('titles');
-//       table.integer('location_id').unsigned().references('id').inTable('locations');
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
+db.knex.schema.hasTable('role_job').then(function (exists) {
+  if (!exists) {
+    db.knex.schema.createTable('role_job', function (table) {
+      table.timestamps();
+      table.uuid('Job_rowId').primary();
+      table.uuid('Role_rowId').primary();
+    }).then(function (table) {
+      console.log('Create Table', table);
+    });
+  }
+});
 
-// db.knex.schema.hasTable('companies_locations').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('companies_locations', function (table) {
-//       table.integer('company_id').unsigned().references('id').inTable('companies');
-//       table.integer('location_id').unsigned().references('id').inTable('locations');
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
+db.knex.schema.hasTable('skill_job').then(function (exists) {
+  if (!exists) {
+    db.knex.schema.createTable('skill_job', function (table) {
+      table.timestamps();
+      table.uuid('Job_rowId').primary();
+      table.uuid('Skill_rowId').primary();
+    }).then(function (table) {
+      console.log('Create Table', table);
+    });
+  }
+});
+
+db.knex.schema.hasTable('startup_job').then(function (exists) {
+  if (!exists) {
+    db.knex.schema.createTable('startup_job', function (table) {
+      table.timestamps();
+      table.uuid('Job_rowId').primary();
+      table.uuid('Startup_rowId').primary();
+    }).then(function (table) {
+      console.log('Create Table', table);
+    });
+  }
+});
 
 module.exports = db;
