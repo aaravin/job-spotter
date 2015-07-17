53ffbbe23186770404a751b3b52a5d88ff2ead0f
Paul Sokolik <paul.sokolik@gmail.com>
2015-07-16 21:10:41 -0700

    add initial styling to navbar; make room for D3 charts

--- client/views/navigationView.js
+++ client/views/navigationView.js
@@ -5,11 +5,13 @@ var Nav = React.createClass({
   
   render: function() {
     return (
-      <div className="nav nav-bar">
-        <h1>JobSpotter</h1>
-        <a href="#">Signup</a>
-        <a href="#">Login</a>
-        <a href="#">About</a>
+      <div className="navbar navbar-inverse navbar-static-top container-fliud" id="navigation">
+        <h1 className="navbar-brand">JobSpotter</h1>
+        <ul className="nav nav-pills navbar-nav navbar-right">
+          <li><a href="#">Signup</a></li>
+          <li><a href="#">Login</a></li>
+          <li><a href="#">About</a></li>
+        </ul>
       </div>
     );
   }
