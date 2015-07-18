ce2208849bb7f689b0c694defb779fce9d4a3069
Timmy Luong <tcl.luong@gmail.com>
2015-07-18 15:13:27 -0700

    Remove oauthKeys.js

--- server/oauthKeys.js
+++
@@ -1,9 +0,0 @@
-var ids = {};
-
-ids.linkedin = {
-  clientID: '75d922y49ikdio',
-  clientSecret: 'imB0FZsZlSwXw6DQ',
-  callbackURL: "http://localhost:8080/auth/linkedin/callback"
-};
-
-module.exports = ids;
