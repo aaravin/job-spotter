bd902b070f2465302d57c961b4e593f311b23cde
Paul Sokolik <paul.sokolik@gmail.com>
2015-07-20 15:07:19 -0700

    change li to ul in jobs list

--- client/views/jobView.js
+++ client/views/jobView.js
@@ -7,11 +7,11 @@ var JobListing = React.createClass({
 
   render: function() {
     return (
-    <li className="job-item">
+    <ul className="job-item">
       <span className="job-title">{this.props.joblisting.get('title')} - </span>
       <span className="job-company">{this.props.joblisting.get('company')} - </span>
       <span className="job-location">{this.props.joblisting.get('locClient')}</span>
-    </li>)
+    </ul>)
   }
 });
 
