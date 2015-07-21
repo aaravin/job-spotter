ba7695d65be20330a6d1cab6fe976665cdeb80a8
Paul Sokolik <paul.sokolik@gmail.com>
2015-07-20 19:23:57 -0700

    build SQL query functions for title and location in filter controller

--- server/controllers/filterController.js
+++ server/controllers/filterController.js
@@ -1,43 +1,84 @@
 var Promise = require('bluebird');
-var Loc = require('../../legacy_sqldb/models/loc');
-var Job = require('../../legacy_sqldb/models/job');
-var Startup = require('../../legacy_sqldb/models/startup');
-var db = require('../../legacy_sqldb/config.js');
+var Location = require('../../db/models/location');
+var Link = require('../../db/models/link');
+var Company = require('../../db/models/company');
+var db = require('../../db/config.js');
+var _ = require('underscore');
 
 module.exports = {
-  getJobsWithLocation: function (req, res, next) {
-    console.log("Sending jobs to /api/jobs/city", req.query.cityName);
-    var allJobs = [];
-    var locClient;
 
-    db.knex.select('Jobs.id as id', 'Jobs.name as title', 'Startups.name as company', 'Jobs.salary_min as salmin', 'Jobs.salary_max as salmax', 'Locs.name as city').from('Locs')
-    .leftJoin('loc_job', 'Locs.id', 'loc_job.Loc_rowId')
-    .leftJoin('Jobs', 'Jobs.id', 'loc_job.Job_rowId')
-    .leftJoin('startup_job', 'Jobs.id', 'startup_job.Job_rowId')
-    .leftJoin('Startups', 'Startups.id', 'startup_job.Startup_rowId')
-    .where('Locs.name', req.query.cityName)
-    .then(function (jobs) {
-      //if Washington, DC, don't use normal regex because it removes substring after ','
-      if (jobs[0].city === "washington,_dc") {
-        locClient = "Washington, DC";
-      } else {
-        locClient = jobs[0].city.replace(/_/g, " ").replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-      }
-      for (var i = 0; i < jobs.length; i++) {
-        var job = {
-          job_id: jobs[i].id,
-          title: jobs[i].title,
-          salary: (jobs[i].salmin + jobs[i].salmax)/2,
-          locServer: jobs[i].city,
-          locClient: locClient,
-          company: jobs[i].company
-        };
+  buildJobs: function(jobs) {
+    return _.map(jobs, function(job) {
+      var jobInfo = {};
 
-        allJobs.push(job);
-      }
+      jobInfo.link = job.link;
+      jobInfo.skills = job.skills;
+      jobInfo.sal_min = job.sal_min;
+      jobInfo.sal_max = job.sal_max;
+      jobInfo.sal_avg = (job.sal_min + job.sal_max)/2;
+      jobInfo.equity = job.equity;
+      jobInfo.location = job.city;
+      jobInfo.title = job.title;
+      jobInfo.company = job.company;
 
-      res.status(200).send(allJobs);
+      return jobInfo;
     });
-  }
+  },
+
+  getJobsWithLocation: function (req, res, next) {
+    console.log("Sending jobs to /api/jobs/ LOCATION SEARCH:",  req.query.location);
+
+    db.knex.select('Links.id as id', 'Links.link as link', 'Links.skills as skills', \
+                   'Links.sal_min as sal_min', 'Links.sal_max as sal_max', \
+                   'Links.sal_avg as sal_avg', 'Links.equity as equity', 'Locations.city as city', \
+                   'Titles.title as title', 'Companies.name as company', \
+                   ).from('Locations')
+    .leftJoin('Links', 'Links.title_id', 'Locations.id')
+    .leftJoin('Titles', 'Titles.id', 'Links.title_id')
+    .leftJoin('Companies', 'Companies.id', 'Links.company_id')
+    .where('Locations.city', req.query.location)
+    .then(function(jobs) {
+      var jobsData = module.exports.buildJobs(jobs);
+      res.status(200).send(jobsData);
+    }); 
+  },
+
+  getJobsWithTitle: function() {
+    console.log("Sending jobs to /api/jobs/ TITLE SEARCH:",  req.query.title);
+    
+    db.knex.select('Links.id as id', 'Links.link as link', 'Links.skills as skills', \
+                   'Links.sal_min as sal_min', 'Links.sal_max as sal_max', \
+                   'Links.sal_avg as sal_avg', 'Links.equity as equity', 'Locations.city as city', \
+                   'Titles.title as title', 'Companies.name as company', \
+                   ).from('Titles')
+    .leftJoin('Links', 'Links.title_id', 'Titles.id')
+    .leftJoin('Locations', 'Locations.id', 'Links.location_id')
+    .leftJoin('Companies', 'Companies.id', 'Links.company_id')
+    .where('Titles.title', req.query.title)
+    .then(function(jobs) {
+      var jobsData = module.exports.buildJobs(jobs);
+      res.status(200).send(jobsData);
+    });
+  }, 
+
+  getJobsWithBoth: function() {
+    console.log("Sending jobs to /api/jobs/ TITLE & LOCATION SEARCH:",  req.query.title, req.query.location);
+    
+    db.knex.select('Links.id as id', 'Links.link as link', 'Links.skills as skills', \
+                   'Links.sal_min as sal_min', 'Links.sal_max as sal_max', \
+                   'Links.sal_avg as sal_avg', 'Links.equity as equity', 'Locations.city as city', \
+                   'Titles.title as title', 'Companies.name as company', \
+                   ).from('Titles')
+    .leftJoin('Links', 'Links.title_id', 'Titles.id')
+    .leftJoin('Locations', 'Locations.id', 'Links.location_id')
+    .leftJoin('Companies', 'Companies.id', 'Links.company_id')
+    .where('Titles.title', req.query.title)
+    andWhere('Locations.city', req.query.location)
+    .then(function(jobs) {
+      var jobsData = module.exports.buildJobs(jobs);
+      res.status(200).send(jobsData);
+    });
+  },
+
 };
 
