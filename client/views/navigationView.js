3a3e0b1fee22789415303c92e22f44ffc1f5d919
Paul Sokolik <paul.sokolik@gmail.com>
2015-07-23 20:02:53 -0700

    fix bug in title form auto-complete

--- client/views/navigationView.js
+++ client/views/navigationView.js
@@ -61,7 +61,7 @@ var Nav = React.createClass({
         $(this).prev('.ui-helper-hidden-accessible').remove();
       },
       close: function(e) {
-        context.setState({location: e.target.value });
+        context.setState({title: e.target.value });
       }
     });   
   },
@@ -73,8 +73,8 @@ var Nav = React.createClass({
           <h1 id="title" className="navbar-brand">JobSpotter</h1>
           <form type="submit" onSubmit={this.handleSubmit} className="navbar-form navbar-middle" id="search-form">
             <div className="form-group">
-              <input type="text" value={this.state.location} onChange={this.searchLocation} placeholder="Enter a Location" ref="location" className="form-control" id="autocomplete"/>
-              <input type="text" value={this.state.title} onChange={this.searchTitle} placeholder="Enter a Job Title" ref="title" className="form-control"/>
+              <input type="text" value={this.state.location} onChange={this.searchLocation} placeholder="Enter a Location" ref="location" className="form-control" id="autocomplete" />
+              <input type="text" value={this.state.title} onChange={this.searchTitle} placeholder="Enter a Position" ref="title" className="form-control" id="autocomplete" />
               <button id="search-button" onClick={this.handleSubmit} className="btn btn-default navbar-btn form-control">Search Jobs</button>
             </div>
           </form>
