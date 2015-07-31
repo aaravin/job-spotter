6745665839e58a434442e00eed0f60e83615da1d
Paul Sokolik <paul.sokolik@gmail.com>
2015-07-30 19:27:41 -0700

    Revert "Fix job count metric"

--- client/views/jobsView.js
+++ client/views/jobsView.js
@@ -11,11 +11,11 @@ var JobsList = React.createClass({
       return <JobListing joblisting={job} key={index} />
     });
 
-    if (this.props.location !== null && this.props.title !== null && this.props.location !== '' && this.props.title === '') {
+    if (this.props.location !== '' && this.props.title === '') {
       titleString = '' + count.toLocaleString() + (count===1 ? ' job ' : ' jobs ') + 'found in ' + this.props.location.toUpperCase();
-    } else if (this.props.location !== null && this.props.title !== null && this.props.location === '' && this.props.title !== '') {
+    } else if (this.props.location === '' && this.props.title !== '') {
       titleString = '' + count.toLocaleString() + (count===1 ? ' job ' : ' jobs ') + 'found related to ' + this.props.title.toUpperCase();
-    } else if (this.props.location !== null && this.props.title !== null && this.props.location !== '' && this.props.title !== '') {
+    } else if (this.props.location !== '' && this.props.title !== '') {
       titleString = '' + count.toLocaleString() + (count===1 ? ' job ' : ' jobs ') + 'found in ' + this.props.location.toUpperCase() + ' related to ' + this.props.title.toUpperCase();
     }
 
