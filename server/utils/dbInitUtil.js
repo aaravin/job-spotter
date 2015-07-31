b84345ca8e74876886e9e0da0d8461318ec78752
Ashwin Aravindan <aaravin@ncsu.edu>
2015-07-30 17:13:49 -0700

    Changed dropping of database to dropping of tables

--- server/utils/dbInitUtil.js
+++ server/utils/dbInitUtil.js
@@ -1,5 +1,6 @@
 var host = process.env.HOST || '127.0.0.1';
 var port = process.env.DBPORT || 3306;
+var dbname = process.env.DBNAME || 'jobs';
 var dbuser = process.env.DBUSER || 'root';
 var dbpassword = process.env.DBPASSWORD || '';
 var dbSchemaUtil = require('./dbSchemaUtil.js');
@@ -12,20 +13,38 @@ module.exports = {
         host: host,
         user: dbuser,
         password: dbpassword,
+        database: dbname,
         charset: 'utf8',
         port: port
       }
     });
 
-    console.log("Dropping database...");
-    knex.raw('DROP DATABASE jobs').then(function() {
-      console.log("Dropped!");
-      console.log("Creating database...");
-      knex.raw('CREATE DATABASE jobs').then(function() {
-        console.log("Created!");
-        console.log("Setting up schema...");
-        dbSchemaUtil.setupSchema();
+    console.log("Dropping tables...");
+    knex.raw('DROP TABLE users').then(function() {
+      knex.raw('DROP TABLE titles').then(function() {
+        knex.raw('DROP TABLE companies').then(function() {
+          knex.raw('DROP TABLE locations').then(function() {
+            knex.raw('DROP TABLE links').then(function() {
+              knex.raw('DROP TABLE titles_companies').then(function() {
+                knex.raw('DROP TABLE titles_locations').then(function() {
+                  knex.raw('DROP TABLE companies_locations').then(function() {
+                    console.log("Dropped!");
+                    console.log("Setting up schema...");
+                    dbSchemaUtil.setupSchema();
+                  })
+                })
+              })
+            })
+          })
+        })
       })
+      // console.log("Dropped!");
+      // console.log("Creating database...");
+      // knex.raw('CREATE DATABASE jobs').then(function() {
+      //   console.log("Created!");
+      //   console.log("Setting up schema...");
+      //   dbSchemaUtil.setupSchema();
+      // })
     });
   }
 };
